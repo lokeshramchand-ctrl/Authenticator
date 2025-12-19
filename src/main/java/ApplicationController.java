f18f47316be351b91121ca735e4283cab4ea0c30
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-12-19 13:42:57 +0530

    Remove welcome endpoint from ApplicationController

--- src/main/java/ApplicationController.java
+++ src/main/java/ApplicationController.java
@@ -1,16 +1,12 @@
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("rest/auth")
 public class ApplicationController {
-    
-    @GetMapping("/welcome")
-    public String welcome()
-    {
-         return "Hey! welcome to GeeksforGeeks";
-    }
+
 
 }
